581ce63a3f9db9dc14344bb6097302b3c8bce5cc
Mikhail Barzilovich <mikhail.barzilovich@hellopay.com>
2017-03-24 17:15:44 +0700

    Rework async channels

--- graphite-render-exporter.go
+++ graphite-render-exporter.go
@@ -13,12 +13,12 @@ import (
 
 
 const pollPeriod = time.Second * 10
-
+var graphiteBaseURL = os.Getenv("GRAPHITE_URL")
+var graphiteTargets = os.Getenv("TARGETS")
 var url string 
 
-var tick = time.NewTicker(pollPeriod).C
-var metricsOut = make(chan []Metric)
-var metricRequest = make(chan bool)
+
+var metricRequest = make(chan chan []Metric)
 
 type Target struct {
   Target string `json:"target"`
@@ -40,9 +40,14 @@ func (g Datapoint) String() string {
 type Metric struct {
   Name string
   Value float64
+  Labels map[string]string
 }
 func (m Metric) String() string {
-  return m.Name + " " + strconv.FormatFloat(m.Value, 'f', -1, 64) + "\n"
+  labelList := []string{}
+  for k, v := range m.Labels {
+    labelList = append(labelList, k+"=\""+v+"\"")
+  }
+  return m.Name + "{" + strings.Join(labelList, ",") + "}" + " " + strconv.FormatFloat(m.Value, 'f', -1, 64) + "\n"
 }
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
@@ -51,7 +56,7 @@ var myClient = &http.Client{Timeout: 10 * time.Second}
 func poller(channel chan []Metric) {
 //  log.Println("Start polling metrics from Graphite")
   metrics := getMetrics(url)
-//  log.Println("Got metrics from Graphite", metrics)
+//  log.Println("Poller: Got metrics from Graphite", metrics)
   channel <- metrics
 //  log.Println("Poll finished")
 }
@@ -64,12 +69,15 @@ func getMetrics(url string) []Metric {
   log.Println(data)
   m := []Metric{}
   for _, t := range data {
-    metric := Metric{Name: strings.Replace(t.Target, ".", "_", -1), Value: getLastNonNullValue(t.Datapoints)}
+    metric := Metric{Name: strings.Replace(t.Target, ".", "_", -1), Value: getLastNonNullValue(t.Datapoints), Labels: getLabels()}
     m = append(m, metric)
   }
   return m
 }
 
+func getLabels() map[string]string {
+  return map[string]string{"url": graphiteBaseURL}
+}
 
 func getLastNonNullValue(d []Datapoint) float64 {
   for i:= len(d)-1 ; i>=0 ; i-- {
@@ -94,11 +102,13 @@ func serveGraphite(w http.ResponseWriter, r *http.Request) {
 		return
 	}
   defer r.Body.Close()
-//  log.Println("Got request")
+  log.Println("Server: Got request")
   w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-  metricRequest <- true
+  var metricsOut = make(chan []Metric)
+  defer close(metricsOut)
+  metricRequest <- metricsOut
   metrics := <- metricsOut
-//  log.Println("Received to handler", metrics)
+  log.Println("Server: Received to handler", metrics)
   c := 0
   for _, m := range metrics {
 //    log.Println("writing metric to response #", c ," - " , m)
@@ -111,16 +121,17 @@ func serveGraphite(w http.ResponseWriter, r *http.Request) {
 func storage() {
   var metrics = []Metric{}
   var metricsIn = make(chan []Metric)
+  var tick = time.NewTicker(pollPeriod).C
   go poller(metricsIn)
   for {
     select {
     case <- tick:
       go poller(metricsIn)
-      case metrics = <- metricsIn:
+    case metrics = <- metricsIn:
 //      log.Println("Received metrics from poller", metrics)
-    case <- metricRequest:
+    case c := <- metricRequest:
 //      log.Println("Received request for metrics")
-      metricsOut <- metrics
+      c <- metrics
 //      log.Println("Metrics sent to handler", metrics)
     }
   }
@@ -128,9 +139,7 @@ func storage() {
 
 
 func main() {
-  var graphiteBaseURL = os.Getenv("GRAPHITE_URL")
   if (graphiteBaseURL == "") { graphiteBaseURL = "http://localhost:8080/render" }
-  var graphiteTargets = os.Getenv("TARGETS")
   if (graphiteTargets == "") { graphiteTargets = "*.*" }
   var graphiteParameters = "target="+graphiteTargets+"&from=-50s&format=json"
   // stats_counts.*
